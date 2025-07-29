6450439a6bf66936393a13e208761ba943989a62
doxuyenxo <doxuyen.md@gmail.com>
2025-07-30 01:07:48 +0700

    Few update

--- timekeeping_system/app/core/database.py
+++ timekeeping_system/app/core/database.py
@@ -1,31 +1,35 @@
-
 import sys
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 # Add project root to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 def get_database():
-  load_dotenv()
-  connection = os.getenv("MONGO_CONNECTION_STRING")
+    load_dotenv()
+    connection = os.getenv("MONGO_CONNECTION_STRING")
 
-  if not connection:
-    raise ValueError("Please setup database connection string!")
+    if not connection:
+        raise ValueError("Please setup database connection string!")
 
-  myclient = MongoClient(connection)
-  mydb = myclient["timekeeping"] #In MongoDB, a database is not created until it gets content!
-  return mydb
+    myclient = MongoClient(connection)
+    mydb = myclient[
+        "timekeeping"
+    ]  # In MongoDB, a database is not created until it gets content!
+    return mydb
+
 
 def get_employee_collection(mydb):
-  mycol = mydb["employee"]
-  return mycol
+    mycol = mydb["employee"]
+    return mycol
+
 
 def get_timekeeping_tracking_collection(mydb):
-  mycol = mydb["timekeeping_tracking"]
-  return mycol
+    mycol = mydb["timekeeping_tracking"]
+    return mycol
+
 
 if __name__ == "__main__":
-  pass
+    pass
